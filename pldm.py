29216393c243f8f9754f451a134d639d99076654
bradenmweight <braden.m.weight@gmail.com>
2020-08-22 19:41:09 -0400

    Create density matrix observable definition

--- pldm.py
+++ pldm.py
@@ -3,7 +3,7 @@ import model
 
 # Initialization of the mapping Variables
 def initMapping(Nstates, initState = 0, stype = "focused"):
-    global qF, qB, pF, pB
+    #global qF, qB, pF, pB, qF0, qB0, pF0, pB0
     qF = np.zeros((Nstates))
     qB = np.zeros((Nstates))
     pF = np.zeros((Nstates))
@@ -35,9 +35,22 @@ def propagateMapVars(qF, qB, pF, pB, dt): # Note that I removed "R" from this li
           pF[i] -= 0.5 * dt * np.sum(Vij[i,:] * qF[:])             
     return qF, qB, pF, pB
 
-initMapping(2)
-propagateMapVars(qF, qB, pF, pB, 1)
+def getPopulation(qF, qB, pF, pB, qF0, qB0, pF0, pB0):
+    rho = np.zeros(( len(qF), len(qF) ), dtype=complex) # Define density matrix
+    rho0 = (qF0 - 1j*pF0) * (qB0 + 1j*pB0)
+    for i in range(len(qF)):
+       for j in range(len(qF)):
+          rho[i,j] = 0.25 * (qF[i] + 1j*pF[i]) * (qB[j] - 1j*pB[j]) * rho0
+    return rho
 
+initState = 0
+qF, qB, pF, pB = initMapping(2,initState)
+qF0, qB0, pF0, pB0 = qF[initState], qB[initState], pF[initState], pB[initState] # Set initial values
+
+file01 = open("output.txt","w")
+for i in range(1000):
+   qF, qB, pF, pB = propagateMapVars(qF, qB, pF, pB, 1)
+   
 
 # q(t+dt) = q(t) + 0.25 * SUM_J(vij*Pj)*dt + (1/8) SUM_j(-0.25 SUM_(jk) vjk*qk)*dt^2
 
