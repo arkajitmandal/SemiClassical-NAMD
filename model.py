0f448d3b76dd93730fa17d59a2eb6a82b267865f
bradenmweight <braden.m.weight@gmail.com>
2020-08-24 15:24:06 -0400

    Added new model definition to construct N-state manifold. Also, changed the output of the rhos.

--- model.py
+++ model.py
@@ -1,17 +1,29 @@
 #def param():
 
 import numpy as np
+from scipy.linalg import toeplitz
 
 class parameters():
-   dt = 0.01
-   NSteps = 10000
+   NSteps = int(2*10**6)
+   dt = 2*10**(-4)
 
-def Hel():
-    v = np.zeros((2,2))
-    VMat[0,0] = 0.1
+def HelTwoLevel():
+    VMat = np.zeros((2,2))
+    VMat[0,0] = 0.1 # Hartrees (27.2114 eV/Hartree)
     VMat[1,0] = 0.01
     VMat[0,1] = 0.01
     VMat[1,1] = 0.0
     return VMat
 
-#def dHel():
+def HelEqualManifold(N = 3):
+    zeros = np.zeros((N-2))
+    matrixvec = [0,0.01]
+    matrixvec.extend(zeros)
+    print ([0,0.01].extend(zeros))
+    VMat = toeplitz(matrixvec,matrixvec)
+    for n in range(N):
+        for m in range(N):
+            if (m == n):
+                VMat[n,m] += 0.2 * (1 - n/N)
+    print (VMat)
+    return VMat
--- pldm.py
+++ pldm.py
@@ -24,11 +24,10 @@ def initMapping(Nstates, initState = 0, stype = "focused"):
     return qF, qB, pF, pB 
 
 def propagateMapVars(qF, qB, pF, pB, dt): # Note that I removed "R" from this list temporarily
-    VMat = model.Hel() # Get interaction Hamiltonian from model file
     qFin, qBin, pFin, pBin = qF, qB, pF, pB # Store input position and momentum for verlet propogation
     # Store initial array containing sums to use at second derivative step
-    VMatxqB =  np.array([np.sum(VMat[k,:] * qBin[:]) for k in range(N)])
-    VMatxqF =  np.array([np.sum(VMat[k,:] * qFin[:]) for k in range(N)])
+    VMatxqB =  np.array([np.sum(VMat[k,:] * qBin[:]) for k in range(NStates)])
+    VMatxqF =  np.array([np.sum(VMat[k,:] * qFin[:]) for k in range(NStates)])
     for i in range(NStates): # Loop over q's and p's for initial update of positions
        pB[i] -= 0.5 * dt * np.sum(VMat[i,:] * qBin[:]) ## First Derivatives ##
        pF[i] -= 0.5 * dt * np.sum(VMat[i,:] * qFin[:])
@@ -38,39 +37,55 @@ def propagateMapVars(qF, qB, pF, pB, dt): # Note that I removed "R" from this li
            qB[i] -= (dt**2/2.0) * (VMat[i,k])* VMatxqB[k] ## Second Derivatives ##
            qF[i] -= (dt**2/2.0) * (VMat[i,k])* VMatxqF[k]
 
-    for i in range(N): # Loop over q's and p's for final update of fictitious momentum
-       pB[i] -= 0.5 * dt * np.sum(Vij[i,:] * qB[:])
-       pF[i] -= 0.5 * dt * np.sum(Vij[i,:] * qF[:])
+    for i in range(NStates): # Loop over q's and p's for final update of fictitious momentum
+       pB[i] -= 0.5 * dt * np.sum(VMat[i,:] * qB[:])
+       pF[i] -= 0.5 * dt * np.sum(VMat[i,:] * qF[:])
     return qF, qB, pF, pB
 
-def getPopulation(qF, qB, pF, pB, qF0, qB0, pF0, pB0):
+def getPopulation(qF, qB, pF, pB, qF0, qB0, pF0, pB0, step):
+    print (step/NSteps * 100, "%")
     rho = np.zeros(( len(qF), len(qF) ), dtype=complex) # Define density matrix
     rho0 = (qF0 - 1j*pF0) * (qB0 + 1j*pB0)
+    file01.write(str(step))
+    file03.write(str(step))
     for i in range(len(qF)):
        for j in range(len(qF)):
-          if (j >= i):
+          if (j >= i and i != j):
               rho[i,j] = 0.25 * (qF[i] + 1j*pF[i]) * (qB[j] - 1j*pB[j]) * rho0
+              file01.write("\t" + str(rho[i,j].real))
+              if (i == j):
+                file03.write("\t" + str(rho[i,j].real))
+              if (i == j and i == NStates-1):
+                file03.write("\t" + str(np.sum(rho[i,i].real for i in range(len(rho)))) + "\n")
+                file01.write("\n")
+
+
     return rho
 
 
 ## Start Main Program
 
-initState = 0
-qF, qB, pF, pB = initMapping(2,initState)
-qF0, qB0, pF0, pB0 = qF[initState], qB[initState], pF[initState], pB[initState] # Set initial values of fictitious oscillator variables
+#VMat = model.HelTwoLevel() # Get interaction Hamiltonian from model file
+VMat = model.HelEqualManifold() # NEW EXPERIMENT ~BMW
+NStates = len(VMat) # This is the dimension of the interaction hamiltonian.
+initState = 0 # Choose (arbitrarily???) the initial state of the particle population. To see "relaxation" of particle population, should be high-energy state in VMat.
+qF, qB, pF, pB = initMapping(NStates,initState) # Call function to initialize fictitious oscillators according to focused ("Default") or according to gaussian random distribution
+qF0, qB0, pF0, pB0 = qF[initState], qB[initState], pF[initState], pB[initState] # Set initial values of fictitious oscillator variables for future use
 
-file01 = open("output_rho.txt","w") # Density natrix: Step, 11, 12, 13, 14, 22, 23, 24, 33, 34, 44, or similar for uuper triangle of NxN matrix
+file01 = open("output_rho.txt","w") # Density natrix: Step, 11, 12, 13, 14, 22, 23, 24, 33, 34, 44, or similar for upper triangle of NxN matrix, SUM_OF_DIAGS
+file03 = open("output_rho_diags.txt","w")
 #file02 = open("output_map.txt","w") # Fictitious Oscillator Motion (Use for sanity check): Step, qF[0], PF
 
 for i in range(NSteps):
-   rho_current = getPopulation(qF, qB, pF, pB, qF0, qB0, pF0, pB0)
-   file01.write( str(i) + "\t" + "\t".join(rho_current.flatten().real.astype("str"))+ "\n")
-   #file02.write( str(i) + "\t" + str(qF[0]) + "\t" + str(pF[0]) + + "\t" + str(qB[0]) + "\t" + str(pB[0]) + "\n")
-   qF, qB, pF, pB = propagateMapVars(qF, qB, pF, pB, dt)
+    if (i % 1000 == 0):
+        rho_current = getPopulation(qF, qB, pF, pB, qF0, qB0, pF0, pB0, i)
+    qF, qB, pF, pB = propagateMapVars(qF, qB, pF, pB, dt)
 file01.close()
 #file02.close()
-   
 
+## OLD CODE: ##   
+#file01.write( str(i) + "\t" + "\t".join(rho_current.flatten().real.astype("str")) + "\t" + str(np.sum(rho_current[i,i].real for i in range(len(rho_current)))) + "\n")
+#file02.write( str(i) + "\t" + str(qF[0]) + "\t" + str(pF[0]) + + "\t" + str(qB[0]) + "\t" + str(pB[0]) + "\n")
 
 
 
