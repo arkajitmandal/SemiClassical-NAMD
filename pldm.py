54ee1ba30c04a81540c921e4824bb9583d62410c
arkajitmandal <arkajitmandal@gmail.com>
2021-06-26 19:34:23 -0400

    A lot of refactoring

--- pldm.py
+++ pldm.py
@@ -5,6 +5,10 @@ import spinBoson as model
 from spinBoson import Hel, dHel, initR, dHel0
 import random
 
+class Bunch:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
 # Initialization of the mapping Variables
 def initMapping(Nstates, initState = 0, stype = "focused"):
     #global qF, qB, pF, pB, qF0, qB0, pF0, pB0
@@ -25,7 +29,7 @@ def initMapping(Nstates, initState = 0, stype = "focused"):
     return qF, qB, pF, pB 
 
 def Umap(qF, qB, pF, pB, dt, VMat):
-    qFin, qBin, pFin, pBin = qF, qB, pF, pB # Store input position and momentum for verlet propogation
+    qFin, qBin, pFin, pBin = qF * 1.0, qB * 1.0, pF * 1.0, pB * 1.0 # Store input position and momentum for verlet propogation
     # Store initial array containing sums to use at second derivative step
 
     VMatxqB =  VMat @ qBin #np.array([np.sum(VMat[k,:] * qBin[:]) for k in range(NStates)])
@@ -47,34 +51,59 @@ def Umap(qF, qB, pF, pB, dt, VMat):
 
     return qF, qB, pF, pB
 
-def Force(R, qF, qB, pF, pB):
-    dH = dHel(R) # Nxnxn Matrix, N = Nuclear DOF, n = NStates 
-    dH0 = dHel0(R)
-    F = np.zeros((len(R)))
-    F -= dH0
+def Force(dat):
+
+    dH = dat.dHij #dHel(R) # Nxnxn Matrix, N = Nuclear DOF, n = NStates 
+    dH0 = dat.dH0
+    qF, pF, qB, pB =  dat.qF, dat.pF, dat.qB, dat.pB
+    # F = np.zeros((len(dat.R)))
+    F = -dH0
     for i in range(len(qF)):
         F -= 0.25 * dH[i,i,:] * ( qF[i] ** 2 + pF[i] ** 2 + qB[i] ** 2 + pB[i] ** 2)
         for j in range(i+1, len(qF)):
             F -= 0.5 * dH[i,j,:] * ( qF[i] * qF[j] + pF[i] * pF[j] + qB[i] * qB[j] + pB[i] * pB[j])
     return F
 
-def VelVer(R, P, qF, qB, pF, pB, dtI, dtE, F1, Hij,M=1): # Ionic position, ionic velocity, etc.
-    v = P/M
-    EStep = int(dtI/dtE)
-    # Hij = Hel(R)
+def VelVer(dat) : # R, P, qF, qB, pF, pB, dtI, dtE, F1, Hij,M=1): # Ionic position, ionic velocity, etc.
+ 
+    # data 
+    qF, qB, pF, pB = dat.qF, dat.qB, dat.pF, dat.pB
+    par =  dat.param
+    v = dat.P/par.M
+    EStep = int(par.dtN/par.dtE)
+    
+    # half-step mapping
     for t in range(EStep):
-        qF, qB, pF, pB = Umap(qF, qB, pF, pB, dtE/2, Hij)
-    #F1 = Force(R, qF, qB, pF, pB)
-    R += v * dtI + 0.5 * F1 * dtI ** 2 / M
-    Hij = Hel(R)
-    for t in range(EStep):
-        qF, qB, pF, pB = Umap(qF, qB, pF, pB, dtE/2, Hij)
-    F2 = Force(R, qF, qB, pF, pB)
-    v += 0.5 * (F1 + F2) * dtI / M
-    return R, v*M, qF, qB, pF, pB, F2, Hij
+        qF, qB, pF, pB = Umap(qF, qB, pF, pB, par.dtE/2, dat.Hij)
+    
 
-def pop(qF, qB, pF, pB, rho0):
-    return np.outer(qF + 1j*pF, qB-1j*pB) * rho0
+    # ======= Nuclear Block ==================================
+    F1    =  Force(dat) # force with {qF(t+dt/2)} * dH(R(t))
+    dat.R += v * par.dtN + 0.5 * F1 * par.dtN ** 2 / par.M
+    
+    
+    dat.Hij  = Hel(dat.R)
+    dat.dHij = dHel(dat.R)
+    dat.dH0  = dHel0(dat.R)
+    
+    F2 = Force(dat) # force with {qF(t+dt/2)} * dH(R(t+ dt))
+    v += 0.5 * (F1 + F2) * par.dtN / par.M
+
+    dat.P = v * par.M
+    # =======================================================
+    
+    # half-step mapping
+    dat.Hij = Hel(dat.R)
+    for t in range(EStep):
+        qF, qB, pF, pB = Umap(qF, qB, pF, pB, par.dtE/2, dat.Hij)
+    dat.qF, dat.qB, dat.pF, dat.pB = qF, qB, pF, pB 
+
+    return dat
+
+
+
+def pop(dat):
+    return np.outer(dat.qF + 1j * dat.pF, dat.qB-1j*dat.pB) * dat.rho0
 
 def runTraj(parameters = model.parameters):
     #------- Seed --------------------
@@ -84,43 +113,44 @@ def runTraj(parameters = model.parameters):
         pass
     #------------------------------------
     ## Parameters -------------
-    dtE = parameters.dtE
-    dtN = parameters.dtN
     NSteps = parameters.NSteps
     NTraj = parameters.NTraj
     NStates = parameters.NStates
-    M = parameters.M #mass
     initState = parameters.initState # intial state
     stype = parameters.stype
     nskip = parameters.nskip
     #---------------------------
-
+    
     rho_ensemble = np.zeros((NStates,NStates,NSteps//nskip), dtype=complex)
     # Ensemble
     for itraj in range(NTraj): 
-        R,P = initR()
+        # Trajectory data
+        dat = Bunch(param = parameters)
+        dat.R, dat.P = initR()
+
+        # set propagator
         vv  = VelVer
 
-        # Call function to initialize fictitious oscillators 
-        # according to focused ("Default") or according 
-        # to gaussian random distribution
-        qF, qB, pF, pB = initMapping(NStates, initState, stype) 
+        # Call function to initialize mapping variables
+        dat.qF, dat.qB, dat.pF, dat.pB = initMapping(NStates, initState, stype) 
 
         # Set initial values of fictitious oscillator variables for future use
-        qF0, qB0, pF0, pB0 = qF[initState], qB[initState], pF[initState], pB[initState] 
-        rho0 = 0.25 * (qF0 - 1j*pF0) * (qB0 + 1j*pB0)
+        qF0, qB0, pF0, pB0 = dat.qF[initState], dat.qB[initState], dat.pF[initState], dat.pB[initState] 
+        dat.rho0 = 0.25 * (qF0 - 1j*pF0) * (qB0 + 1j*pB0)
 
-        #----- Initial Force --------
-        F1 = Force(R, qF, qB, pF, pB)
-        Hij = Hel(R)
-        iskip = 0
+        #----- Initial QM --------
+        dat.Hij  = Hel(dat.R)
+        dat.dHij = dHel(dat.R)
+        dat.dH0  = dHel0(dat.R)
+        #----------------------------
+        iskip = 0 # please modify
         for i in range(NSteps): # One trajectory
             #------- ESTIMATORS-------------------------------------
             if (i % nskip == 0):
-                rho_ensemble[:,:,iskip] += pop(qF, qB, pF, pB, rho0)
+                rho_ensemble[:,:,iskip] += pop(dat)
                 iskip += 1
             #-------------------------------------------------------
-            R, P, qF, qB, pF, pB, F1, Hij = vv(R, P, qF, qB, pF, pB, dtN, dtE, F1, Hij, M)
+            dat = vv(dat)
 
     return rho_ensemble
 
