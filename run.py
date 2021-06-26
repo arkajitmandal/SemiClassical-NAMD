220bc3cd57f064eae03072df8485c5ffa5520fa1
arkajitmandal <arkajitmandal@gmail.com>
2021-06-26 17:29:03 -0400

    bug fixes

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import os,sys 
 fold = int(sys.argv[1]) 
 os.system(f"rm -rf RUN")
+os.mkdir("RUN")
 for i in range(fold):
-    try:
-        os.mkdir(f"RUN/run-{i}")
-    except:
-        pass
+
+    os.mkdir(f"RUN/run-{i}")
     os.system(f"sbatch parallel.py RUN/run-{i}")
     
