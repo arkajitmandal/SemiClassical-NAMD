e3300a8c5a89614a4cdf11e75cb3cf5a2cf9a8cc
Sutirthachowdhury <schowdh4@ur.rochester.edu>
2021-09-19 16:46:29 -0400

    added ring-polymer sampling in bead rep.

--- Method/nrpmd.py
+++ Method/nrpmd.py
@@ -25,3 +25,52 @@ def initMap(param):
             p[i,ib] = η * np.sin(θ) 
     return p,q
 #============================================================
+
+#===============intializing nuclear positions with Monte-carlo sampling===========
+def monte_carlo(param, steps = 3000, dR = 0.5):
+    R = np.zeros((param.ndof,param.nb))
+    ndof, nb = R.shape
+    βn = param.beta/nb 
+
+    #Monte carlo loop
+    for i in range(steps):
+        rDof  = np.random.choice(range(ndof))
+        rBead = np.random.choice(range(nb))
+
+        # Energy before Move
+        En0 = ringPolymer(R[rDof,:], param) + Hel0(R[rDof,rBead]) 
+
+         # update a bead -------------
+        dR0 = dR * (random() - 0.5)
+        R[rDof, rBead] += dR0
+        #----------------------------
+        # Energy after Move
+        En1 = ringPolymer(R[rDof,:], param) + Hel0(R[rDof,rBead]) 
+
+        # Probality of MC
+        Pr = np.min([1.0,np.exp(-βn * (En1-En0))])
+        # a Random number
+        r = random()
+        # Accepted
+        if r < Pr:
+            pass
+        # Rejected
+        else:
+            R[rDof, rBead] -= dR0
+    return R
+
+
+def ringPolymer(R,param):
+    """
+    Compute Ringpolymer Energy
+    E = ∑ 0.5  (m nb^2/β^2)  (Ri-Ri+1)^2
+    """
+    nb = param.nb
+    βn = (param.beta)/nb 
+    Ω  = (1 / βn)  
+    M  = param.M
+    E = 0
+    for k in range(-1,nb-1):
+        E+= 0.5 * M * Ω**2 * (R[k] - R[k+1])**2
+    return E
+#==========================================================
