591a17c764a3af0e7e2edc725f1f17281f18a576
Arkajit Mandal <arkajitmandal@gmail.com>
2024-04-18 17:23:59 -0500

    simplifying code

--- avg.py
+++ avg.py
@@ -4,53 +4,48 @@ import numpy as np
 from glob import glob
 print("-"*50)
 
-def getInput(input,key):
-    txt = [i for i in input if i.find(key)!=-1][0].split("=")[1].split("#", 1)[0].replace("\n","")
-    return txt.replace(" ","")
-
 try:
-    input = open(sys.argv[1], 'r').readlines()
-    print(f"Reading {sys.argv[1]} for average")
+    fold = sys.argv[1]
 except:
-    print("Reading input.txt for average")
-    input = open('input.txt', 'r').readlines()
+    fold = "output"
+
 
 try:
     filename = sys.argv[2]
     filenames = [filename]
 except:
     print ("Averaging all .txt files")
-    filenames = [i.replace("RUN/run-0/","") for i in glob("RUN/run-0/*.txt")]
-    print (filenames)
-
-try:
-    fold = int(sys.argv[3])
-except:
-    print (f"Detected {len(glob('RUN/run-*'))} folders")
-    fold = len(glob("RUN/run-*"))
-    if fold == 0:
-        print("No outputs found!")
+    fnames = glob(f"{fold}/*.txt")
+    # get name from filenames: {name}-{number}.txt 
+    # the name itself could include a dash
+    names = ["-".join(f.split("/")[-1].split("-")[:-1]) for f in fnames]
+    filenames = list(set(names))
+    
+    print(f"Averaging files that have the name:")
+    for f in filenames:
+        print(f"{f}-*.txt")
+ 
         
 for filename in filenames:
     try:
-        outName = filename 
+        outName = filename + ".txt"
 
-        dirs = [f"RUN/run-{i}" for i in  range(fold)]
-
-        dat = np.loadtxt(dirs[0]+"/"+filename) 
-        for i in range(1, fold):
-            dat += np.loadtxt(dirs[i]+"/"+filename)
-        method = getInput(input,"Method")
-        if method.find('sqc')!= -1:
+        fnames = glob(f"{fold}/{filename}-*.txt")
+        dat = 0
+        for i in fnames:
+            dat += np.loadtxt(i)
+        N = len(fnames)
+        if i.find('sqc')!= -1:
             norm = np.sum(dat[:,1:], axis=1)
             for t in range(len(dat[:,0])):
                 norm = np.sum(dat[t,1:])
                 dat[t,1:] = dat[t,1:]/norm
-                dat[t,0]  = dat[t,0]/fold
+                dat[t,0]  = dat[t,0]/N
             np.savetxt(outName, dat)
         else:
-            np.savetxt(outName, dat/fold)
-    except:
-        pass
+            np.savetxt(outName, dat/N)
+        print ("Averaging done!")
+    except Exception as e:
+        print(f"Could not average {filename}-*.txt :")
+        print(e)
 
-print ("Gathered all data, feel free to remove the folder 'RUN'")
